eb7ab41116f57872c4287829110d1172cba356f1
scottsc0de <scott5902@gmail.com>
2022-08-07 12:26:34 -0400

    feature/add-js

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+/*
+var topic = "HTML";
+if (topic === "HTML") {
+    console.log("Let's study HTML!");
+}
+else if (topic === "CSS") {
+    console.log("Let's study CSS!");
+}
+else if (topic === "Git") {
+    console.log("Let's study Git!");
+}
+else if (topic === "Javascript") {
+    console.log("Let's study Javascript!");
+}
+else {
+    console.log("Please try again!");
+}
+
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+
+for (var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
+*/
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+
